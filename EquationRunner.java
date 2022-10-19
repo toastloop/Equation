3c6730041d426d75c4a5e47645e7e9aff6849bd8
toastloop <toastloop@gmail.com>
2022-10-18 20:40:21 -0600

    v0.0.1

---
+++ EquationRunner.java
@@ -0,0 +1,6 @@
+public class EquationRunner {
+    public static void main(String[] args) {
+        Equation equation = new Equation();
+        equation.setValueTo("eleven").plus("ten").minus("eight").times("five").raisedToThePowerOf("two").dividedBy("one hundred").toTheLogOfBase("two").equals();
+    }
+}
